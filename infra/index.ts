6db063700c7fd0842a356a315561cfe00c5ee45b
g-bibek <bibek@omnidek.com>
2022-11-19 20:28:24 -0500

    location changed to east us 2

--- infra/index.ts
+++ infra/index.ts
@@ -5,11 +5,13 @@ import * as azure_native from "@pulumi/azure-native";
 const config = new pulumi.Config();
 const stack = pulumi.getStack();
 
-const resourceGroup = new resources.ResourceGroup(`bk-portal-ui-${stack}`);
+const resourceGroup = new resources.ResourceGroup(`bk-portal-ui-${stack}`, {
+  location: "eastus2"
+});
 
 const staticSite = new azure_native.web.StaticSite("staticSite", {
   branch: "infra/pulumi-setup",
-  location: "UkSouth",
+  location: resourceGroup.location,
   name: `${stack}-ui`,
   repositoryUrl: "https://github.com/byoma-kusuma/core.x.ui",
   repositoryToken: config.require("gh_token"),
