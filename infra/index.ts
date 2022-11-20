c26a435807c71ba8586edc474bc762c957f3221a
g-bibek <bibek@omnidek.com>
2022-11-19 19:55:07 -0500

    pulumi setup

---
+++ infra/index.ts
@@ -0,0 +1,22 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as resources from "@pulumi/azure-native/resources";
+import * as azure_native from "@pulumi/azure-native";
+
+const config = new pulumi.Config();
+const stack = pulumi.getStack();
+
+const resourceGroup = new resources.ResourceGroup(`bk-portal-ui-${stack}`);
+
+const staticSite = new azure_native.web.StaticSite("staticSite", {
+  branch: "infra/pulumi-setup",
+  location: "UkSouth",
+  name: `${stack}-ui`,
+  repositoryUrl: "https://github.com/byoma-kusuma/core.x.ui",
+  repositoryToken: config.require("gh_token"),
+  resourceGroupName: resourceGroup.name,
+  sku: {
+    name: "Free",
+    tier: "Free"
+  }
+});
+export const url = staticSite.contentDistributionEndpoint;
