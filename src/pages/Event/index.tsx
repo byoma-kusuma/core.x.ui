cebf4488cb2e735e32a283e752453dca7d3d3d04
Amogh Rijal <xamoghx@gmail.com>
2023-01-28 19:52:22 +0545

    Event setup

---
+++ src/pages/Event/index.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@mui/material";
+import * as React from "react";
+import { useNavigate } from "react-router-dom";
+import PageWithHeader from "../../components/PageWithHeader";
+
+export default function Events() {
+  const navigate = useNavigate();
+  return (
+    <PageWithHeader
+      header="Events"
+      crumbs={[
+        { label: "Events", route: "/app/events", key: "1" },
+        { label: "List", route: "/", key: "2" }
+      ]}
+      actionButton={
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => navigate("/app/events/new")}
+        >
+          Create New Event
+        </Button>
+      }
+    >
+      Events page
+    </PageWithHeader>
+  );
+}
