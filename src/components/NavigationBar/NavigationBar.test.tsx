a3b8e3d1932be3ba4802c1e944be8d24d0c38019
Amogh Rijal <xamoghx@gmail.com>
2023-06-02 21:43:29 +0545

    Add test and story for navigation bar

---
+++ src/components/NavigationBar/NavigationBar.test.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import NavigationBar from ".";
+// eslint-disable-next-line react/display-name
+jest.mock("../SearchBar", () => (props: { onSearch: (v: string) => void }) => (
+  <input
+    type="text"
+    onChange={(e) => props.onSearch(e.target.value)}
+    placeholder="Search"
+  />
+));
+
+describe("<NavigationBar />", () => {
+  const setup = () => {
+    const onHamburgerClick = jest.fn();
+    const onSearch = jest.fn();
+    const stackItemsRenderer = jest.fn().mockReturnValue(<div>Item</div>);
+    render(
+      <NavigationBar
+        onHamburgerClick={onHamburgerClick}
+        onSearch={onSearch}
+        stackItemsRenderer={stackItemsRenderer}
+      />
+    );
+
+    return { onHamburgerClick, onSearch, stackItemsRenderer };
+  };
+
+  it("renders without error", () => {
+    setup();
+    const buttons = screen.getAllByRole("button");
+    expect(buttons[0]).toBeInTheDocument(); // Assuming the first button is the hamburger icon.
+  });
+
+  it("responds to hamburger click", () => {
+    const { onHamburgerClick } = setup();
+    const buttons = screen.getAllByRole("button");
+    fireEvent.click(buttons[0]);
+    expect(onHamburgerClick).toHaveBeenCalled();
+  });
+
+  it("calls onSearch when input in searchbar changes", () => {
+    const { onSearch } = setup();
+    fireEvent.change(screen.getByPlaceholderText("Search"), {
+      target: { value: "Testing" }
+    });
+    expect(onSearch).toHaveBeenCalledWith("Testing");
+  });
+
+  it("renders stack items", () => {
+    setup();
+    expect(screen.getByText("Item")).toBeInTheDocument();
+  });
+});
