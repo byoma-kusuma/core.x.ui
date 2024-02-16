1a807e08f95bb604c01baccc46ae2db7ad48a278
g-bibek <bibek@omnidek.com>
2024-02-15 23:10:23 -0500

    member details code structure revamp password reset integration with backend

---
+++ src/containers/member/memberFormContainerComponents/MemberUserSection.tsx
@@ -0,0 +1,152 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { LoadingButton } from "@mui/lab";
+import { Box, Grid, Link, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import PhotoUploader from "components/PhotoUploader";
+import {
+  useCreateUserMutation,
+  useRequestPasswordResetMutation
+} from "generated/graphql";
+import { useConfirm } from "material-ui-confirm";
+import React from "react";
+import GqlApiHandler from "services/GqlApiHandler";
+
+export interface MemberUserSectionProps {
+  id: number;
+  doesMemberHasUser: boolean;
+  memberUserName?: string;
+  memberUserRoleName?: string;
+  memberName: string | null;
+}
+
+const UserInfoItemStyle = styled("div")(() => ({
+  width: "100%"
+}));
+
+const MemberUserSection: React.FC<MemberUserSectionProps> = (props) => {
+  const [{ fetching: creatingUser }, createUserMut] = useCreateUserMutation();
+  const [{ fetching: resettingPassword }, passwordResetMut] =
+    useRequestPasswordResetMutation();
+
+  const confirm = useConfirm();
+
+  const handleUserCreate = (memberId: number, memberName: string) => {
+    confirm({
+      description: (
+        <Typography>
+          Are you sure you want to create an administrator for the user{" "}
+          {memberName}?
+        </Typography>
+      ),
+      title: "Confirm admin creation",
+      confirmationButtonProps: {
+        color: "primary",
+        variant: "contained"
+      },
+      confirmationText: "Confirm"
+    }).then(async () => {
+      new GqlApiHandler(
+        await createUserMut({
+          createUserInput: { memberId }
+        })
+      )
+        .onSuccess(({ notiSuccess }) => {
+          notiSuccess("User successfully created!");
+        })
+        .onError(({ notiErr }) => notiErr());
+    });
+  };
+
+  const handleUserPasswordReset = (
+    passwordResetProps: MemberUserSectionProps
+  ) => {
+    confirm({
+      description: (
+        <Typography>
+          Are you sure you want to reset the password for the user{" "}
+          {passwordResetProps.memberUserName}?
+        </Typography>
+      ),
+      title: "Confirm password reset",
+      confirmationButtonProps: {
+        color: "primary",
+        variant: "contained"
+      },
+      confirmationText: "Confirm"
+    }).then(async () => {
+      new GqlApiHandler(
+        await passwordResetMut({
+          userName: passwordResetProps.memberUserName!
+        })
+      )
+        .onSuccess(({ notiSuccess }) => {
+          notiSuccess(
+            `Password reset instructions successfully sent to ${passwordResetProps.memberUserName}!`
+          );
+        })
+        .onError(({ notiErr }) => notiErr());
+    });
+  };
+  return (
+    <>
+      <Grid item xs={12} md={6} lg={4}>
+        <PhotoUploader height={"calc(100vh - 200px)"}>
+          {props.id && (
+            <UserInfoItemStyle style={{ paddingBottom: "16px" }}>
+              <Typography variant="subtitle1">User</Typography>
+              {props.doesMemberHasUser ? (
+                <Box
+                  display="flex"
+                  justifyContent="space-between"
+                  alignItems="flex-start"
+                >
+                  <Box>
+                    <Link component="button" variant="subtitle2">
+                      {props.doesMemberHasUser}
+                    </Link>
+                    <br />
+                    <Typography variant="body2" fontSize={12}>
+                      {props.memberUserRoleName}
+                    </Typography>
+                  </Box>
+                  <LoadingButton
+                    variant="contained"
+                    size="small"
+                    sx={{ ml: "8px" }}
+                    loading={resettingPassword}
+                    onClick={() => {
+                      handleUserPasswordReset(props);
+                    }}
+                  >
+                    Reset Password
+                  </LoadingButton>
+                </Box>
+              ) : (
+                <Box display="flex" justifyContent="space-between">
+                  <Typography variant="body2" color="GrayText">
+                    No user has been created for this member.
+                  </Typography>
+                  <Box>
+                    <LoadingButton
+                      variant="contained"
+                      size="small"
+                      sx={{ ml: "8px" }}
+                      loading={creatingUser}
+                      onClick={() => {
+                        handleUserCreate(props.id, props.memberName as string);
+                      }}
+                    >
+                      Create
+                    </LoadingButton>
+                  </Box>
+                </Box>
+              )}
+            </UserInfoItemStyle>
+          )}
+        </PhotoUploader>
+      </Grid>
+    </>
+  );
+};
+
+export default MemberUserSection;
---
+++ src/schemas/memberDetailsFormikSchema.ts
@@ -0,0 +1,157 @@
+import { FormikSchema } from "utils/formik/formikUtils";
+import * as Yup from "yup";
+
+export const memberDetailsFormikSchema = {
+  title: {
+    type: "text",
+    required: false,
+    initialValue: ""
+  },
+  firstName: {
+    type: "text",
+    required: true,
+    initialValue: "",
+    validator: Yup.string().min(2).required("Required")
+  },
+  lastName: {
+    type: "text",
+    required: true,
+    initialValue: "",
+    validator: Yup.string().min(2).required("Required")
+  },
+  middleName: {
+    type: "text",
+    required: false,
+    initialValue: ""
+  },
+  active: {
+    type: "boolean",
+    required: true,
+    initialValue: true
+  },
+  isMember: {
+    type: "boolean",
+    required: true,
+    initialValue: true
+  },
+  email: {
+    type: "text",
+    required: true,
+    initialValue: "",
+    validator: Yup.string().email("Invalid email").required("Required")
+  },
+  phoneMobile: {
+    type: "tel",
+    required: false,
+    initialValue: ""
+  },
+  phoneLand: {
+    type: "tel",
+    required: false,
+    initialValue: ""
+  },
+  phoneOther: {
+    type: "tel",
+    required: false,
+    initialValue: ""
+  },
+  centreId: {
+    type: "number",
+    required: true,
+    initialValue: null,
+    validator: Yup.number().integer().required("Required")
+  },
+  yearOfBirth: {
+    type: "number",
+    required: false,
+    initialValue: null
+  },
+  membershipType: {
+    type: "text",
+    required: false,
+    initialValue: null
+  },
+  gender: {
+    type: "text",
+    required: false,
+    initialValue: null
+  },
+  sanghaJoinDate: {
+    type: "date",
+    required: false,
+    initialValue: null
+  },
+  viber: {
+    type: "text",
+    required: false,
+    initialValue: ""
+  },
+  messenger: {
+    type: "text",
+    required: false,
+    initialValue: ""
+  },
+  insta: {
+    type: "text",
+    required: false,
+    initialValue: ""
+  },
+  note: {
+    type: "text",
+    required: false,
+    initialValue: ""
+  },
+  currentStreetAddress: {
+    type: "text",
+    required: false,
+    initialValue: "",
+    label: "Current Street Address"
+  },
+  permanentStreetAddress: {
+    type: "text",
+    required: false,
+    initialValue: "",
+    label: "Permanent Street Address"
+  },
+  currentCity: {
+    type: "text",
+    required: false,
+    initialValue: "",
+    label: "Current City"
+  },
+  permanentCity: {
+    type: "text",
+    required: false,
+    initialValue: "",
+    label: "Permanent City"
+  },
+  currentStateProvince: {
+    type: "text",
+    required: false,
+    initialValue: "",
+    label: "Current State/Province"
+  },
+  permanentStateProvince: {
+    type: "text",
+    required: false,
+    initialValue: "",
+    label: "Permanent State/Province"
+  },
+  currentCountry: {
+    type: "text",
+    required: true,
+    initialValue: "",
+    label: "Current Country"
+  },
+  permanentCountry: {
+    type: "text",
+    required: true,
+    initialValue: "",
+    label: "Permanent Country"
+  },
+  refugeName: {
+    type: "text",
+    required: false,
+    initialValue: ""
+  }
+} as FormikSchema;
